1be13d1445fec9a1abb31c2e0007eca6ac4dbd06
kitzer <kitzer04@mail.com>
2026-05-24 18:54:15 -0500

    Definición estructural del sistema, inicio de partida con arquitetura de Puertos y Adaptadores y patrón DDD.

---
+++ Application/Configuration/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        //Inyección de Dependencias
+        
+        return services;
+    }
+}
---
+++ Infraestructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infraestructure.Configuration;
+
+
+public static class DependencyInjection
+{
+    // IServiceCollection funciona con EFCore
+    public static IServiceCollection Addinfraestructure(this IServiceCollection services)
+    {
+        //Inyección de Dependencias
+        
+        return services;
+    }
+}
