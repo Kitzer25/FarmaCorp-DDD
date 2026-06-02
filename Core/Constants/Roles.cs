e2393bf3bb15218a37e17a0f29ea31c75dec2a17
BorixQ <borixquiroz@gmail.com>
2026-06-02 00:39:29 -0500

    feat(B-01-04): Borix - Definir constantes estáticas para roles del sistema

---
+++ Core/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Constants;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Pharmacist = "Pharmacist";
+    public const string SalesRep = "SalesRep";
+    public const string Manager = "Manager";
+}
